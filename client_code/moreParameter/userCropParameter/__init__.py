81ff4fb02c62fa59e2eba3a0de81d1f08d2a05d4
cxshimmer@163.com <cxshimmer@163.com>
2025-06-06 16:26:31 +0000

    Edited moreParameter.userCropParameter

--- client_code/moreParameter/userCropParameter/__init__.py
+++ client_code/moreParameter/userCropParameter/__init__.py
@@ -5,10 +5,15 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from aquacrop import Crop
 class userCropParameter(userCropParameterTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the component is clicked."""
+    
+    parameter=c
