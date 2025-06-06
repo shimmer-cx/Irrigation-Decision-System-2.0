0412e21d6a0c910608c33a5f13b8d5e9bcd8ff00
cxshimmer@163.com <cxshimmer@163.com>
2025-06-06 15:33:09 +0000

    Edited moreParameter

--- client_code/moreParameter/__init__.py
+++ client_code/moreParameter/__init__.py
@@ -2,13 +2,21 @@ from ._anvil_designer import moreParameterTemplate
 from anvil import *
 import anvil.server
 import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+import copy
+# import anvil.tables as tables
+# import anvil.tables.query as q
+# from anvil.tables import app_tables
 
 class moreParameter(moreParameterTemplate):
+  
+
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_1.items=[]
+    crop_info=anvil.server.call('get_zhiKaiKou_info')
+    for crop in crop_info['crop_infor']:
+      tempRow=[{'user':crop_info['User']['email'],'cropName':crop['cropName']}]
+      self.repeating_panel_1.items= self.repeating_panel_1.items + copy.deepcopy( tempRow ) 
 
     # Any code you write here will run before the form opens.
