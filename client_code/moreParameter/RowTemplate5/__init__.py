5f91bf88db6f6888dc2dddc16f1786b832f3afdb
cxshimmer@163.com <cxshimmer@163.com>
2025-06-06 14:41:22 +0000

    Created moreParameter.userCropParameter and 作物参数.png; Edited moreParameter and moreParameter.RowTemplate5

--- client_code/moreParameter/RowTemplate5/__init__.py
+++ client_code/moreParameter/RowTemplate5/__init__.py
@@ -1,10 +1,10 @@
 from ._anvil_designer import RowTemplate5Template
 from anvil import *
-import anvil.server
-import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+# import anvil.server
+# import anvil.users
+# import anvil.tables as tables
+# import anvil.tables.query as q
+# from anvil.tables import app_tables
 
 class RowTemplate5(RowTemplate5Template):
   def __init__(self, **properties):
@@ -12,3 +12,7 @@ class RowTemplate5(RowTemplate5Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the component is clicked."""
+    open_form('moreParameter.userCropParameter')
