4ae5783d6f7b5a18ce1f5d12f16e4eb23a71ac80
cxshimmer@163.com <cxshimmer@163.com>
2025-06-14 02:40:24 +0000

    Edited ServerModule2

--- server_code/ServerModule2.py
+++ server_code/ServerModule2.py
@@ -278,7 +278,6 @@ def RunModel(current_user):
      
       water_content =list(  water_flux['Wr'])#'Wr作物根区水分'
       # actual_transpiration =list(  water_flux['Tr'])#作物蒸腾量（mm）。
-
       # new_Row['actual_transpiration']=[round(x, 2) for x in actual_transpiration]
       new_Row['irrigation']=irrigation
       new_Row['water_content']=[round(x, 2) for x in water_content]
